36b8df7816c3823fe0257baa28cf18054a7f2fcc
Jeffcwng <ng.jeffrey@gmail.com>
2014-09-18 12:48:58 -0700

    add sub folder, models, admin.py

--- lab_app/urls.py
+++ lab_app/urls.py
@@ -7,4 +7,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'wanderful.views.home', name='home'),
+
+
+
+    
 )
